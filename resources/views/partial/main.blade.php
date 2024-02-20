703714775e864d2ad2bead7ae94ed3e22ef5da80
lutfi1506 <lutfirahman248@gmail.com>
2024-02-20 07:54:17 +0700

    menambah bootstrap

--- resources/views/partial/main.blade.php
+++ resources/views/partial/main.blade.php
@@ -5,10 +5,12 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>{{ $title }}</title>
+    <link rel="stylesheet" href="css/bootstrap.css">
     <link rel="stylesheet" href="css/mystyle.css">
 </head>
 <body>
     @yield('container')
+    <script src="js/bootstrap.js"></script>
     <script src="js/myscript.js"></script>
 </body>
 </html>
